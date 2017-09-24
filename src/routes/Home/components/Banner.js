6923e7bfd09cde2dc3b944ee86efd2b8db67f8a6
zhouyinian <bewithyouzyn@163.com>
2017-09-24 17:20:31 +0800

    更改图片引入路径,打包进项目

--- src/routes/Home/components/Banner.js
+++ src/routes/Home/components/Banner.js
@@ -4,15 +4,18 @@
 import React from 'react'
 
 import {Carousel} from 'antd';
+import banner1 from '../../../assets/images/banner_house.jpg';
+import banner2 from '../../../assets/images/banner_mogu.jpg';
+import banner3 from '../../../assets/images/banner_wall.jpg';
 export const Banner = ()=>(
     <div className="carousel">
         <Carousel effect="fade" autoplay easing="ease-in-out" autoplaySpeed="3500" speed="500">
             <div className="banner_item"
-                 style={{backgroundImage: `url(src/assets/images/banner_house.jpg)`}}></div>
+                 style={{backgroundImage: `url(${banner1})`}}></div>
             <div className="banner_item"
-                 style={{backgroundImage: `url(src/assets/images/banner_mogu.jpg)`}}></div>
+                 style={{backgroundImage: `url(${banner2})`}}></div>
             <div className="banner_item"
-                 style={{backgroundImage: `url(src/assets/images/banner_wall.jpg)`}}></div>
+                 style={{backgroundImage: `url(${banner3})`}}></div>
         </Carousel>
     </div>
 )
